393bf0a93ba190f3c41401534394d42653d02a04
Kai Chung Tse <kaichungtse@Kais-MacBook-Pro.local>
2025-05-26 22:23:16 +0800

    Fix ValidatorTests.shouldNotValidateWhenFirstNameEmpty and apply formatting

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,8 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("notempty");
+		// person.setFirstName("notempty");
+		person.setFirstName("");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
